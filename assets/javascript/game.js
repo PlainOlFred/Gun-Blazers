107e46378342a1328920f2fee24aba6f76e16f6b
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-08-12 14:16:34 -0500

    created object for each playable character

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+//Characters
+let warrior = {
+    name: 'player 1',
+    hp: 200,
+    attack: 5,
+    defend: 5
+};
+
+let sorcerer= {
+    name: 'Player 2',
+    hp: 70,
+    attack: 10,
+    defend: 10
+};
+
+let elf = {
+    name: 'Player 3',
+    hp: 500,
+    attack: 2,
+    defend: 2
+};
+
+let wizard = {
+    name: 'Player 4',
+    hp: 100,
+    attack: 5,
+    defend: 2
+};
+
+let rouge = {
+    name: 'Player 5',
+    hp: 80,
+    attack: 5,
+    defend: 20
+};
+
+
+let choosePlayerArray = [];
+let chooseEnemyArray = [];
+let enemy = [];
+let defeatedEnemy = []
+
+let fight = function() {
+    
+
+};
+
+let heal = function( ){
+
+};
