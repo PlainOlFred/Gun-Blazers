3643e8b514b6c2f0f8de5cedeb1458d497ca329d
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-11-29 20:01:50 -0600

    clean code for readability;

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,35 +20,42 @@ $(document).ready(function(){
         }
     }
 
-    let warrior = createPlayer('Warrior', 200, 2, 1, 2, 'assets/images/warrior.jpg')
-    let sorcerer = createPlayer('Sorcerer', 120, 3, 2, 3, 'assets/images/sorcerer.jpg')
-    let elf = createPlayer('Elf', 100, 1, 1, 1, 'assets/images/elf.jpg')
-    let wizard = createPlayer('Wizard', 80, 6, 3, 6, 'assets/images/wizard.jpg')
-    let rouge = createPlayer('Rouge', 180, 2, 7, 2, 'assets/images/rouge2.jpg')
-   
+    // Create Players
+    let 
+        warrior = createPlayer('Warrior', 200, 2, 1, 2, 'assets/images/warrior.jpg'),
+        sorcerer = createPlayer('Sorcerer', 120, 3, 2, 3, 'assets/images/sorcerer.jpg'),
+        elf = createPlayer('Elf', 100, 1, 1, 1, 'assets/images/elf.jpg'),
+        wizard = createPlayer('Wizard', 80, 6, 3, 6, 'assets/images/wizard.jpg'),
+        rouge = createPlayer('Rouge', 180, 2, 7, 2, 'assets/images/rouge2.jpg')
+    
 
-    let players = [warrior, sorcerer, elf, , rouge, wizard]
+    let players = [warrior, sorcerer, elf, rouge, wizard];
+
     let playersCount= players.length;
-    let playerCards = []
+    
 
 
     //player select variable
-    let playerSelect = false;
-    let opponentSelect= false;
+    let 
+        playerSelect 
+        opponentSelect= false;
     
     //Action variables 
-    let playerHp = 1;
-    let playerAttack = 0;
-    let playerDefend = 0;
-
-    let opponentName = ' '
-    let opponentHp = 1;
-    let opponentCounterAttack = 0;
+    let 
+        playerHp = 1;
+        playerAttack = 0,
+        playerDefend = 0,
+        opponentName = ' '
+        opponentHp = 1;
+        opponentCounterAttack = 0;
 
     //create player card
-    $('#instructionsText').text('Choose Your Player')
+    $('#instructionsText').text('Choose Your Player');
+    let playerCards = [];
+
     players.forEach(function(player){
-        let playerCard = $(`<div id="playerId${player.name}">
+        let  playerCard = $(
+        `<div id="playerId${player.name}">
             <div class="playerCardName">
                 ${player.name}
             </div> 
@@ -62,23 +69,30 @@ $(document).ready(function(){
         </div>`
         );
 
-        playerCard.addClass('playerCard')
-
-        //need to access Data
-        playerCard.attr('dataName', player.name)
-        playerCard.attr('dataHp', player.hp) 
-        playerCard.attr('dataAttack', player.attack)
-        playerCard.attr('dataCounterAttack', player.counterAttack)
-        playerCard.attr('dataDefend', player.defend)
+        //Player Card Data
+        playerCard.attr({
+            'class': 'playerCard',
+            'dataName': player.name,
+            'dataHp': player.hp,
+            'dataAttack': player.attack,
+            'dataCounterAttack': player.counterAttack,
+            'dataDefend': player.defend
+        })
+       
         
         playerCards.push(playerCard) 
     });
+
+    
     //print cards 
     $('#choosePlayerLine').append(playerCards)
     
 
+
+
+
     //click to choose player
-    $('.playerCard').click(function(){
+    $('#choosePlayerLine').on('click', '.playerCard', function() {
             $('#instructionsText').text('Choose Your Opponent')
             if(!playerSelect){
 
@@ -86,13 +100,14 @@ $(document).ready(function(){
                 playerAttack =$(this).attr('dataAttack');
                 playerCounterAttack =$(this).attr('dataCounterAttack');
                 playerDefend =$(this).attr('dataDefend');
-                
-                $('#yourPlayerLine').prepend($(this));
+
+                $('#yourPlayerText').text('Your Player');
+                $('#yourPlayerLine').append($(this));
+
                 playersCount --;
-                console.log(playersCount)
+               
                 $(this).unbind();  
 
-                $('#yourPlayerText').text('Your Player')
                 playerSelect = true;
                 
 
@@ -105,17 +120,25 @@ $(document).ready(function(){
                 opponentDefend =$(this).attr('dataDefend');
 
                 $('#yourOpponentText').text('Your Opponent');
+                $('#yourOpponentLine').append($(this));
+
+                //Show Story Box
                 $('#storyBox').show();
                 $('button').show();
-                $('#yourOpponentLine').append($(this));
+                
                 playersCount --;
-                console.log(playersCount)
+
+                $(this).unbind(); 
+
+                opponentSelect = true;
+                
 
                 $('button').css({'display': 'inline-block', 'clear': 'both'})
                 $('button').prop('disabled', false)
                 $('#firstBreak').show()
-                opponentSelect = true;
-                $(this).unbind(); 
+
+                
+                
 
             } 
         
